6c1a5d6a48e5803abfadcefcccd66255e36dd139
RazerFord <desk10567@gmail.com>
2022-08-19 23:40:11 +0700

    add base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Interfaces\ServiceInterface;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Routing\Controller;
 
-class Controller extends BaseController
+class BaseController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Class of service.
+     * 
+     * @var ServiceInterface $service Class of service
+     */
+    protected $service;
 }
